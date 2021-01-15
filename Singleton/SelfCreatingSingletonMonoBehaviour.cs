bc4e19ee6f0f661551b4f3606ec1b11d128543d9
arvidsson <arvidsson@gmail.com>
2021-01-15 20:43:09 +0100

    Added singletons.

---
+++ Singleton/SelfCreatingSingletonMonoBehaviour.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+namespace UnityLib.Singleton
+{
+    /// <summary>
+    /// Self-creating Singleton MonoBehaviour.
+    /// </summary>
+    public class SelfCreatingSingletonMonoBehaviour<T> : MonoBehaviour where T : SelfCreatingSingletonMonoBehaviour<T>
+    {
+        protected static T instance;
+
+        public bool Exists => instance != null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    UpdateInstance();
+                }
+
+                return instance;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (instance == this)
+            {
+                instance = null;
+            }
+        }
+
+        private static void UpdateInstance()
+        {
+            var instances = FindObjectsOfType<T>();
+
+            if (instances.Length == 1)
+            {
+                instance = instances[0];
+            }
+            else if (instances.Length == 0)
+            {
+                instance = new GameObject(typeof(T).Name).AddComponent<T>();
+            }
+            else
+            {
+                Debug.LogError("Found too many instances (" + instances.Length + ") of singleton " + typeof(T).Name);
+            }
+        }
+    }
+}
