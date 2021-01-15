90bce9860ff85cc48adac0a213c1e94a4589adea
arvidsson <arvidsson@gmail.com>
2021-01-15 20:57:51 +0100

    Added Component.

---
+++ Component.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace UnityLib
+{
+    /// <summary>
+    /// A Component is a MonoBehaviour that has easy access to a Root MonoBehaviour that can either be on the same GameObject, or in a parent GameObject.
+    /// </summary>
+    public class Component<T> : MonoBehaviour
+    {
+        public T Root
+        {
+            get
+            {
+                if (!root)
+                {
+                    root = GetComponent<T>();
+                }
+
+                if (!root)
+                {
+                    root = GetComponentInParent<T>();
+                }
+
+                return root;
+            }
+        }
+
+        private T root;
+    }
+}
