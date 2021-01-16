cfd0d5591ea4e3bc7f5314c0b960e820fe22392c
arvidsson <arvidsson@gmail.com>
2021-01-16 12:14:04 +0100

    Added CanvasExtensions

---
+++ Extensions/CanvasExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityEngine
+{
+    public static class CanvasExtensions
+    {
+        /// <summary>
+        /// Returns the scaled position so that we can place things correctly in scaled resolutions, mostly used for figuring out the correct mouse position.
+        /// </summary>
+        public static Vector2 ScreenToScaledPoint(this Canvas canvas, Vector2 position)
+        {
+            return new Vector2(position.x / canvas.scaleFactor, position.y / canvas.scaleFactor);
+        }
+    }
+}
