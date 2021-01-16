fef6663b51bde0e7b37d8bac240786d1d3618f6d
arvidsson <arvidsson@gmail.com>
2021-01-16 16:13:04 +0100

    Added Sprite Sorter

---
+++ Rendering/SpriteSortBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace UnityLib
+{
+    /// <summary>
+    /// All sprites with this behaviour will be sorted by the sprite sorter, so that sprites with lower y-coord will be displayed behind other sprites with higher y-coord.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteSortBehaviour : MonoBehaviour
+    {
+        private SpriteRenderer spriteRenderer;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void OnEnable()
+        {
+            SpriteSorter.RegisterSprite(spriteRenderer);
+        }
+
+        private void OnDisable()
+        {
+            SpriteSorter.UnregisterSprite(spriteRenderer);
+        }
+    }
+}
