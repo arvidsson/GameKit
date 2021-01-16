afa83e248379e8f5c96e13c578c1b3e18de561a1
arvidsson <arvidsson@gmail.com>
2021-01-16 16:25:40 +0100

    Added SimpleAnimationBehaviour

---
+++ Rendering/SimpleAnimationBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace UnityLib
+{
+    /// <summary>
+    /// Simple animation using multiple sprites and showing them in a sequence.
+    /// </summary>
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SimpleAnimationBehaviour : MonoBehaviour
+    {
+        public List<Sprite> frames;
+        [Tooltip("Delay between switching frames")]
+        public float animationDelay = 0.5f;
+
+        private SpriteRenderer spriteRenderer;
+        private int animationIndex = 0;
+        private float animationTimer;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animationTimer = Random.Range(0f, animationDelay);
+        }
+
+        private void Update()
+        {
+            if (Time.time > animationTimer)
+            {
+                animationIndex = ++animationIndex >= frames.Count ? 0 : animationIndex;
+                spriteRenderer.sprite = frames[animationIndex];
+                animationTimer = Time.time + animationDelay;
+            }
+        }
+    }
+}
