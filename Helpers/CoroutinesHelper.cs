acbb953e05a76bb62b51b172f76664a4b98e2e91
arvidsson <arvidsson@gmail.com>
2021-01-16 11:28:01 +0100

    Added CoroutinesHelper

---
+++ Helpers/CoroutinesHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace UnityLib.Helpers
+{
+    /// <summary>
+    /// Makes it possible for any class to make use of coroutines.
+    /// </summary>
+    public static class CoroutinesHelper
+    {
+        private class CoroutineRunner : MonoBehaviour { }
+
+        private static MonoBehaviour runner;
+
+        /// <summary>
+        /// Runs a coroutine. Creates the global coroutine runner if there is none.
+        /// </summary>
+        /// <param name="coroutine">The coroutine you want to run</param>
+        /// <returns>The coroutine that was started.</returns>
+        public static Coroutine Run(IEnumerator coroutine)
+        {
+            if (runner == null)
+            {
+                runner = new GameObject("CoroutineRunner").AddComponent<CoroutineRunner>();
+                MonoBehaviour.DontDestroyOnLoad(runner);
+            }
+
+            return runner.StartCoroutine(coroutine);
+        }
+    }
+}
