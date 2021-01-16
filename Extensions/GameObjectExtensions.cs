d3c7925b1ba4f2eab23a898b5f844bf93718b134
arvidsson <arvidsson@gmail.com>
2021-01-16 11:28:10 +0100

    Added GameObjectExtensions

---
+++ Extensions/GameObjectExtensions.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+namespace UnityEngine
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// Returns a component but checking the result first via an assert.
+        /// </summary>
+        public static T GetRequiredComponent<T>(this GameObject gameObject)
+        {
+            T result = gameObject.GetComponent<T>();
+
+            Debug.Assert(result != null, "Failed to get component: " + typeof(T).ToString(), gameObject);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns true if the gameobject has a specific component.
+        /// </summary>
+        public static bool HasComponent<T>(this GameObject gameObject)
+        {
+            return (gameObject.GetComponent<T>() as Component) != null;
+        }
+
+        /// <summary>
+        /// Destroys all children belonging to the gameobject.
+        /// </summary>
+        public static void DestroyChildren(this GameObject gameObject)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                GameObject.Destroy(child.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Destroys all children immediately belonging to the gameobject.
+        /// </summary>
+        public static void DestroyChildrenImmediately(this GameObject gameObject)
+        {
+            foreach (Transform child in gameObject.transform)
+            {
+                GameObject.DestroyImmediate(child.gameObject);
+            }
+        }
+
+        /// <summary>
+        /// Returns all children belonging to the gameobject.
+        /// </summary>
+        public static GameObject[] GetAllChildren(this GameObject gameObject)
+        {
+            int count = gameObject.transform.childCount;
+            GameObject[] children = new GameObject[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                Transform child = gameObject.transform.GetChild(i);
+                children[i] = child.gameObject;
+            }
+
+            return children;
+        }
+
+        /// <summary>
+        /// Returns a child by name if found, otherwise null.
+        /// </summary>
+        public static Transform Find(this GameObject gameObject, string name)
+        {
+            return gameObject.transform.Find(name);
+        }
+
+        /// <summary>
+        /// Returns a child by name but first check if the child was found via an assert.
+        /// </summary>
+        public static Transform FindRequired(this GameObject gameObject, string name)
+        {
+            return gameObject.transform.FindRequired(name);
+        }
+    }
+}
