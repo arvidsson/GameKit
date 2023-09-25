9be1b3a61fc6b3e0cd285eae7b99fc9bc3c70b8f
arvidsson <arvidsson@gmail.com>
2023-09-25 17:11:23 +0200

    Update FlagsHelper.cs

--- Runtime/Extensions/FlagsHelper.cs
+++ Runtime/Extensions/FlagsHelper.cs
@@ -4,6 +4,7 @@ namespace UnityEngine
 {
     public static class FlagsHelper
     {
+        // bool susanIsIncluded = FlagsHelper.IsSet(names, Names.Susan);
         public static bool IsSet<T>(T flags, T flag) where T : struct
         {
             int flagsValue = (int)(object)flags;
@@ -11,6 +12,7 @@ namespace UnityEngine
             return (flagsValue & flagValue) != 0;
         }
 
+        // FlagsHelper.Set(ref names, Names.Karen);
         public static void Set<T>(ref T flags, T flag) where T : struct
         {
             int flagsValue = (int)(object)flags;
@@ -18,7 +20,8 @@ namespace UnityEngine
 
             flags = (T)(object)(flagsValue | flagValue);
         }
-        
+
+        // FlagsHelper.Unset(ref names, Names.Susan);
         public static void Unset<T>(ref T flags, T flag) where T : struct
         {
             int flagsValue = (int)(object)flags;
