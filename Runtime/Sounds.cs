1fb9b6242b13ce68b0680000eb0204ff57ef6891
arvidsson <arvidsson@gmail.com>
2025-08-02 20:35:01 +0200

    simplified sounds

--- Runtime/Sounds.cs
+++ Runtime/Sounds.cs
@@ -2,22 +2,29 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace GameKit.Audio
+namespace GameKit
 {
-    public class SoundManager : Singleton<SoundManager>
+    // TODO: sound assets scriptable object (single sound, random sound, sequence, spatial etc), spatial sounds, looping sounds etc
+    // so pooling of audio sources using Pool, getting a ref to a sound that is playing so we can stop it and thus return it to the pool
+    public class Sounds : Singleton<Sounds>
     {
-        private List<AudioSource> audioSources;
+        [SerializeField] private int preloadCount = 5;
 
-        public static void PlaySound(AudioClip clip)
+        private List<AudioSource> audioSources = new();
+
+        public static void PlaySound(AudioClip clip, float volume = 1f, float pitch = 1f, bool spatialize = false)
         {
             var audioSource = Instance.GetAudioSourceFromPool();
             audioSource.clip = clip;
+            audioSource.volume = volume;
+            audioSource.pitch = pitch;
+            audioSource.spatialBlend = spatialize ? 1f : 0f;
             audioSource.Play();
         }
 
         protected override void OnSingletonAwake()
         {
-            audioSources = new List<AudioSource>();
+            PreloadAudioSources(preloadCount);
         }
 
         protected override void OnSingletonDestroy()
@@ -33,6 +40,16 @@ namespace GameKit.Audio
                 {
                     ReturnAudioSourceToPool(audioSource);
                 }
+            }
+        }
+
+        private void PreloadAudioSources(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var source = gameObject.AddComponent<AudioSource>();
+                source.enabled = false;
+                audioSources.Add(source);
             }
         }
 
@@ -52,6 +69,7 @@ namespace GameKit.Audio
 
         private void ReturnAudioSourceToPool(AudioSource audioSource)
         {
+            audioSource.clip = null;
             audioSource.enabled = false;
         }
     }
