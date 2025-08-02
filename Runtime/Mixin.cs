578f3b3eb716d12bb306e1974bb641822bf86422
arvidsson <arvidsson@gmail.com>
2025-08-02 12:58:43 +0200

    moved mixin

--- Runtime/Mixin.cs
+++ Runtime/Mixin.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
 
-namespace GameKit.Pattern
+namespace GameKit
 {
+    /// <summary>
+    /// A Mixin is an object that adds behaviour to a Mix object (eg fire damage to a sword).
+    /// </summary>
+    public abstract class Mixin<T> : MonoBehaviour
+    {
+    }
+
     /// <summary>
     /// A Mix is an object which has Mixin components which add flavour to the Mix object. Eg, a sword mix can have different mixins to add fire damage, ice damage etc.
     /// </summary>
