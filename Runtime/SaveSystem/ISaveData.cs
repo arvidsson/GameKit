30676d020ad7da439ad92cb0a8d1c35576e7f4d9
arvidsson <arvidsson@gmail.com>
2025-08-03 15:19:38 +0200

    save data helper

--- Runtime/SaveSystem/ISaveData.cs
+++ Runtime/SaveSystem/ISaveData.cs
@@ -1,7 +1,6 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
 
 namespace GameKit.SaveSystem
 {
@@ -9,5 +8,162 @@ namespace GameKit.SaveSystem
     {
         void Write(BinaryWriter bw);
         void Read(BinaryReader br);
+    }
+
+    public static class SaveDataHelper
+    {
+        #region Lists
+
+        public static void Write<T>(this List<T> binaryDataList, BinaryWriter bw) where T : class, ISaveData, new()
+        {
+            bw.Write(binaryDataList.Count);
+            binaryDataList.ForEach(item => item.Write(bw));
+        }
+
+        public static void Read<T>(this List<T> binaryDataList, BinaryReader br) where T : class, ISaveData, new()
+        {
+            binaryDataList.Clear();
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+                binaryDataList.Add(Create<T>(br));
+        }
+
+        public static void Write<T>(this List<List<T>> binaryDataLists, BinaryWriter bw) where T : class, ISaveData, new()
+        {
+            bw.Write(binaryDataLists.Count);
+            binaryDataLists.ForEach(item => item.Write(bw));
+        }
+
+        public static void Read<T>(this List<List<T>> binaryDataLists, BinaryReader br) where T : class, ISaveData, new()
+        {
+            binaryDataLists.Clear();
+            int listCount = br.ReadInt32();
+            for (int i = 0; i < listCount; i++)
+            {
+                var newList = new List<T>();
+                newList.Read(br);
+                binaryDataLists.Add(newList);
+            }
+        }
+
+        public static void Write(this List<int> binaryDataList, BinaryWriter bw)
+        {
+            bw.Write(binaryDataList.Count);
+            binaryDataList.ForEach(bw.Write);
+        }
+
+        public static void Read(this List<int> binaryDataList, BinaryReader br)
+        {
+            binaryDataList.Clear();
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+                binaryDataList.Add(br.ReadInt32());
+        }
+
+        public static void Write(this List<string> binaryDataList, BinaryWriter bw)
+        {
+            bw.Write(binaryDataList.Count);
+            for (int i = 0; i < binaryDataList.Count; i++)
+                bw.Write(binaryDataList[i] ?? "");
+        }
+
+        public static void Read(this List<string> binaryDataList, BinaryReader br)
+        {
+            binaryDataList.Clear();
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+                binaryDataList.Add(br.ReadString());
+        }
+
+        public static void Write(this List<bool> binaryDataList, BinaryWriter bw)
+        {
+            bw.Write(binaryDataList.Count);
+            for (int i = 0; i < binaryDataList.Count; i++)
+                bw.Write(binaryDataList[i]);
+        }
+
+        public static void Read(this List<bool> binaryDataList, BinaryReader br)
+        {
+            binaryDataList.Clear();
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+                binaryDataList.Add(br.ReadBoolean());
+        }
+
+        #endregion
+
+        #region Dictionaries
+
+        public static void Write<T>(this Dictionary<string, T> binaryDataDictionary, BinaryWriter bw) where T : class, ISaveData, new()
+        {
+            bw.Write(binaryDataDictionary.Count);
+
+            foreach (var entry in binaryDataDictionary)
+                entry.Value.Write(bw);
+        }
+
+        public static void Read<T>(this Dictionary<string, T> binaryDataDictionary, Func<T, string> keySelector, BinaryReader br) where T : class, ISaveData, new()
+        {
+            binaryDataDictionary.Clear();
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+            {
+                T newEntry = Create<T>(br);
+                binaryDataDictionary.Add(keySelector(newEntry), newEntry);
+            }
+        }
+
+        public static void Write<T>(this Dictionary<int, Dictionary<string, T>> instancedDictionary, BinaryWriter bw) where T : class, ISaveData, new()
+        {
+            bw.Write(instancedDictionary.Count);
+
+            foreach (var entry in instancedDictionary)
+            {
+                bw.Write(entry.Key);
+                entry.Value.Write(bw);
+            }
+        }
+
+        public static void Read<T>(this Dictionary<int, Dictionary<string, T>> instancedDictionary, Func<T, string> keySelector, BinaryReader br) where T : class, ISaveData, new()
+        {
+            instancedDictionary.Clear();
+            int instancesCount = br.ReadInt32();
+            for (int i = 0; i < instancesCount; i++)
+            {
+                var newInstance = new Dictionary<string, T>();
+                var instanceId = br.ReadInt32();
+                newInstance.Read(keySelector, br);
+                instancedDictionary.Add(instanceId, newInstance);
+            }
+        }
+
+        public static void Write(this Dictionary<string, int> binaryDataDictionary, BinaryWriter bw)
+        {
+            bw.Write(binaryDataDictionary.Count);
+
+            foreach (var entry in binaryDataDictionary)
+            {
+                bw.Write(entry.Key);
+                bw.Write(entry.Value);
+            }
+        }
+
+        public static void Read(this Dictionary<string, int> binaryDataDictionary, BinaryReader br)
+        {
+            binaryDataDictionary.Clear();
+
+            int itemCount = br.ReadInt32();
+            for (int i = 0; i < itemCount; i++)
+                binaryDataDictionary.Add(br.ReadString(), br.ReadInt32());
+        }
+
+        #endregion
+
+        public static T Create<T>(BinaryReader br) where T : class, ISaveData
+        {
+            T newBinaryData = Activator.CreateInstance<T>();
+            newBinaryData.Read(br);
+            return newBinaryData;
+        }
     }
 }
