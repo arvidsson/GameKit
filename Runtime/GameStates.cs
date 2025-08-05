0e609f1d75efe52f02505ab7054e291ad628ccbb
arvidsson <arvidsson@gmail.com>
2025-08-05 22:04:08 +0200

    fix

--- Runtime/GameStates.cs
+++ Runtime/GameStates.cs
@@ -10,6 +10,14 @@ namespace GameKit
         static readonly Dictionary<System.Type, GameState> states = new();
         static GameState currentState;
 
+        public static void Add(params GameState[] states)
+        {
+            foreach (var state in states)
+            {
+                Add(state);
+            }
+        }
+
         public static void Add(GameState state)
         {
             states[state.GetType()] = state;
@@ -18,6 +26,11 @@ namespace GameKit
         public static void Change<T>() where T : GameState
         {
             CoroutinesHelper.Run(ChangeStateNextFrame<T>());
+        }
+
+        public static void Change(GameState nextState)
+        {
+            CoroutinesHelper.Run(ChangeStateNextFrame(nextState));
         }
 
         public static void Update()
@@ -33,6 +46,7 @@ namespace GameKit
             if (!states.ContainsKey(type))
             {
                 Debug.LogError($"GameStates: GameState {type} has not been added.");
+                yield break;
             }
 
             if (currentState?.GetType() == type)
@@ -40,6 +54,24 @@ namespace GameKit
 
             currentState?.Exit();
             currentState = states[type];
+            currentState.Enter();
+        }
+
+        static IEnumerator ChangeStateNextFrame(GameState nextState)
+        {
+            yield return new WaitForEndOfFrame();
+
+            if (!states.ContainsKey(nextState.GetType()))
+            {
+                Debug.LogError($"GameStates: GameState {nextState.GetType()} has not been added.");
+                yield break;
+            }
+
+            if (currentState == nextState)
+                yield break;
+
+            currentState?.Exit();
+            currentState = nextState;
             currentState.Enter();
         }
     }
