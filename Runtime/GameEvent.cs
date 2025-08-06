6a98cc9ceb43f96aa02fe17a685c5b43970a1926
arvidsson <arvidsson@gmail.com>
2025-08-06 11:22:19 +0200

    fix

--- Runtime/GameEvent.cs
+++ Runtime/GameEvent.cs
@@ -1,4 +1,7 @@
-public abstract class GameEvent { }
-public abstract class GameEvent<T> : GameEvent { }
-public abstract class GameEvent<T1, T2> : GameEvent { }
-public abstract class GameEvent<T1, T2, T3> : GameEvent { }
+namespace GameKit
+{
+    public abstract class GameEvent { }
+    public abstract class GameEvent<T> : GameEvent { }
+    public abstract class GameEvent<T1, T2> : GameEvent { }
+    public abstract class GameEvent<T1, T2, T3> : GameEvent { }
+}
