b99fe82668e254b07609143e72ba44380e6d0a86
arvidsson <arvidsson@gmail.com>
2025-08-06 20:50:37 +0200

    fix

--- Runtime/GameEvents.cs
+++ Runtime/GameEvents.cs
@@ -4,112 +4,36 @@ using System.Collections.Generic;
 namespace GameKit
 {
 
+    public abstract class GameEvent { }
+
     public static class GameEvents
     {
         private static readonly Dictionary<Type, Delegate> handlers = new();
 
-        public static void Register<TEvent>(Action handler) where TEvent : GameEvent
+        public static void AddListener<T>(Action<T> handler) where T : GameEvent
         {
-            var type = typeof(TEvent);
+            var type = typeof(T);
             if (handlers.TryGetValue(type, out var existing))
-                handlers[type] = (Action)existing + handler;
+                handlers[type] = (Action<T>)existing + handler;
             else
                 handlers[type] = handler;
         }
 
-        public static void Unregister<TEvent>(Action handler) where TEvent : GameEvent
+        public static void RemoveListener<T>(Action<T> handler) where T : GameEvent
         {
-            var type = typeof(TEvent);
+            var type = typeof(T);
             if (handlers.TryGetValue(type, out var existing))
             {
-                var updated = (Action)existing - handler;
+                var updated = (Action<T>)existing - handler;
                 if (updated == null) handlers.Remove(type);
                 else handlers[type] = updated;
             }
         }
 
-        public static void Emit<TEvent>() where TEvent : GameEvent
+        public static void Trigger<T>(T args) where T : GameEvent
         {
-            if (handlers.TryGetValue(typeof(TEvent), out var del))
-                ((Action)del)?.Invoke();
-        }
-
-        public static void Register<TEvent, T1>(Action<T1> handler) where TEvent : GameEvent<T1>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-                handlers[type] = (Action<T1>)existing + handler;
-            else
-                handlers[type] = handler;
-        }
-
-        public static void Unregister<TEvent, T1>(Action<T1> handler) where TEvent : GameEvent<T1>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-            {
-                var updated = (Action<T1>)existing - handler;
-                if (updated == null) handlers.Remove(type);
-                else handlers[type] = updated;
-            }
-        }
-
-        public static void Emit<TEvent, T1>(T1 arg) where TEvent : GameEvent<T1>
-        {
-            if (handlers.TryGetValue(typeof(TEvent), out var del))
-                ((Action<T1>)del)?.Invoke(arg);
-        }
-
-        public static void Register<TEvent, T1, T2>(Action<T1, T2> handler) where TEvent : GameEvent<T1, T2>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-                handlers[type] = (Action<T1, T2>)existing + handler;
-            else
-                handlers[type] = handler;
-        }
-
-        public static void Unregister<TEvent, T1, T2>(Action<T1, T2> handler) where TEvent : GameEvent<T1, T2>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-            {
-                var updated = (Action<T1, T2>)existing - handler;
-                if (updated == null) handlers.Remove(type);
-                else handlers[type] = updated;
-            }
-        }
-
-        public static void Emit<TEvent, T1, T2>(T1 arg1, T2 arg2) where TEvent : GameEvent<T1, T2>
-        {
-            if (handlers.TryGetValue(typeof(TEvent), out var del))
-                ((Action<T1, T2>)del)?.Invoke(arg1, arg2);
-        }
-
-        public static void Register<TEvent, T1, T2, T3>(Action<T1, T2, T3> handler) where TEvent : GameEvent<T1, T2, T3>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-                handlers[type] = (Action<T1, T2, T3>)existing + handler;
-            else
-                handlers[type] = handler;
-        }
-
-        public static void Unregister<TEvent, T1, T2, T3>(Action<T1, T2, T3> handler) where TEvent : GameEvent<T1, T2, T3>
-        {
-            var type = typeof(TEvent);
-            if (handlers.TryGetValue(type, out var existing))
-            {
-                var updated = (Action<T1, T2, T3>)existing - handler;
-                if (updated == null) handlers.Remove(type);
-                else handlers[type] = updated;
-            }
-        }
-
-        public static void Emit<TEvent, T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3) where TEvent : GameEvent<T1, T2, T3>
-        {
-            if (handlers.TryGetValue(typeof(TEvent), out var del))
-                ((Action<T1, T2, T3>)del)?.Invoke(arg1, arg2, arg3);
+            if (handlers.TryGetValue(typeof(T), out var handler))
+                ((Action<T>)handler)?.Invoke(args);
         }
     }
 
